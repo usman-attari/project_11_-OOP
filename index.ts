3d945e4fb8687e12d4a36c0ba2b00660ecb2ca0e
muhammad usman <muattari899@gmail.com>
2023-10-24 17:58:35 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+
+
+class Student {
+    name:string
+    constructor(n:string){
+        this.name=n
+    }
+ }
+
+ class Person {
+    students:Student[]=[]
+
+    addstudent(obj:Student){
+        this.students.push(obj)
+    }
+ }
+const persons = new Person ()
+
+const programmStart = async (person:Person ) => {
+    do{console.log("Welcome guest");
+    const ans =await inquirer.prompt ({
+        type:"list",
+        message:"ap kis se bat Kerna pasand Kare ge...",
+        name:"selest",
+        choices:["Khuld se:Self", "student"]
+    });
+    if (ans.selest == "Khuld se:Self" ){
+        console.log("Hello me Khud se baat Ker raha hon");
+        console.log("Meri tabiyat achi hai.");
+     }
+    if (ans.selest == "student" ){
+        const ans =await inquirer.prompt ({
+            type:"input",
+            message:"Ap Ko Kis Student Se Bat Kerni hai",
+            name:"student",
+     })
+        const student = person.students.find(val => val.name ==ans.student)
+        if(!student){
+            const name = new Student(ans.student)
+            person.addstudent(name)
+            
+            console.log(`Hello i am ${name.name}, or me thek hun `);
+            console.log(person.students)
+        };
+
+        if (student){
+            console.log(`Hello i am ${student.name}, or me thek hun....... `);
+            console.log(person.students)
+        }
+    };}while(true)
+}
+programmStart(persons);
